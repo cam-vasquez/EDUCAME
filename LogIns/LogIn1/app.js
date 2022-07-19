804fd2126d95e3b8624326d72a77a61cc744f845
cam-vasquez <00065221@uca.edu.sv>
2022-07-19 21:38:59 +0200

    :sparkles: Falta añadir Validación

--- LogIns/LogIn1/app.js
+++ LogIns/LogIn1/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#password');
 
@@ -8,15 +7,5 @@ togglePassword.addEventListener('click', function (e) {
     password.setAttribute('type', type);
     // toggle the eye / eye slash icon
     this.classList.toggle('bi-eye');
-=======
-const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#password');
-
-togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye / eye slash icon
-    this.classList.toggle('bi-eye');
->>>>>>> 065d2512d34db5b863c8a7d2592fa03527c2f568
-});
+ }
+)
