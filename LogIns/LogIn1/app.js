6c2457bf2174f4a5020a513183db54fdc8adeb80
cam-vasquez <00065221@uca.edu.sv>
2022-07-19 16:27:28 -0600

    :sparkles: Add new

--- LogIns/LogIn1/app.js
+++ LogIns/LogIn1/app.js
@@ -15,11 +15,5 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
 });
 
-togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye / eye slash icon
-    this.classList.toggle('bi-eye');
- }
-)
+
+
