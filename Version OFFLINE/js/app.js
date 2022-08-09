ca28a5ddbbc0b6777d50b073413471498a9a3307
Fernanda Vásquez <82715400+cam-vasquez@users.noreply.github.com>
2022-08-09 21:22:47 +0200

    :clown_face: Befarore everthing is messed up

--- Version OFFLINE/js/app.js
+++ Version OFFLINE/js/app.js
@@ -49,3 +49,66 @@ username.addEventListener("change", () => checkSend(enviar()));
 password.addEventListener("change", () => checkSend(enviar()));
 
 checkSend(enviar());
+
+
+/* let username = document.getElementById("username");
+let password = document.getElementById("password");
+ 
+ 
+      const togglePassword = document.querySelector("#togglePassword");
+
+      togglePassword.addEventListener("click", function () {
+          // toggle the type attribute
+          const type = password.getAttribute("type") === "password" ? "text" : "password";
+          password.setAttribute("type", type);
+
+          // toggle the icon
+          this.classList.toggle("bi-eye");
+      });
+
+      // prevent form submit
+      const form = document.querySelector("form");
+      form.addEventListener('submit', function (e) {
+          e.preventDefault();
+      });
+
+
+
+function checkSend(checker){
+    console.log(checker)
+    if(!checker){
+        document.getElementById("form_registro").addEventListener("submit", function(e){
+            console.log(e);
+            e.preventDefault();
+        });
+    }
+    else{
+        document.getElementById("form_registro").addEventListener("submit", function(e){
+            e.stopPropagation();
+            console.log(e);
+        }, true);
+    }
+}
+
+function enviar(){
+    if((username.value != "" && password.value != "")){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+document.getElementById("submit").onclick = function(){
+    if((username.value != "" && password.value != "")){
+        
+        document.getElementById("alert").style.display = "inline-block";
+    }
+    else{
+        document.getElementById("alert").style.display = "none";
+    }
+}
+
+username.addEventListener("change", () => checkSend(enviar()));
+password.addEventListener("change", () => checkSend(enviar()));
+
+checkSend(enviar()); */
