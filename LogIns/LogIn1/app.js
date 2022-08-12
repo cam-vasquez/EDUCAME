faf6e391ea1cf4f0e2959794622830a1bea264b3
Fernanda Vásquez <82715400+cam-vasquez@users.noreply.github.com>
2022-08-11 22:38:01 -0600

    :sparkles:

--- LogIns/LogIn1/app.js
+++ LogIns/LogIn1/app.js
@@ -1,35 +1,50 @@
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+ 
+ 
+      const togglePassword = document.querySelector("#togglePassword");
+
+      togglePassword.addEventListener("click", function () {
+          // toggle the type attribute
+          const type = password.getAttribute("type") === "password" ? "text" : "password";
+          password.setAttribute("type", type);
+
+          // toggle the icon
+          this.classList.toggle("bi-eye");
+      });
+
+      // prevent form submit
+      const form = document.querySelector("form");
+      form.addEventListener('submit', function (e) {
+          e.preventDefault();
+      });
 
 
-        /* Js para Toggle Password */
-        const togglePassword = document.querySelector("#togglePassword");
-        const password = document.querySelector("#password");
-    
-        togglePassword.addEventListener("click", function () {
-            // toggle the type attribute
-            const type = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type);
-    
-            // toggle the icon
-            //this.classList.toggle("fa-eye");
-            this.classList.toggle("bi-eye");
-            //bi-eye <i class="fa-solid fa-eye"></i>
-        });
-    
-        // prevent form submit
-        const form = document.querySelector("form");
-        form.addEventListener('submit', function (e) {
+function checkSend(checker){
+    console.log(checker)
+    if(!checker){
+        document.getElementById("form_registro").addEventListener("submit", function(e){
+            console.log(e);
             e.preventDefault();
         });
-    
-/* END TOGGLE */
-
-function enviar(){
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    if(username != "" && password != ""){
-       window.open('index.php', '_self');
     }
     else{
-        console.log("Lorem ipsum."); 
+        document.getElementById("form_registro").addEventListener("submit", function(e){
+            e.stopPropagation();
+            console.log(e);
+        }, true);
     }
-} 
+}
+
+function enviar(){
+    if((username.value != "" && password.value != "")){
+        return true;
+    }
+    else
+        return false;
+}
+
+username.addEventListener("change", () => checkSend(enviar()));
+password.addEventListener("change", () => checkSend(enviar()));
+
+checkSend(enviar()); 
