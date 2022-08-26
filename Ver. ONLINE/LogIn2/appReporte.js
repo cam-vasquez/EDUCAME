e5de520f8ac460f8217f32595b64ee8f9e5cbf13
Fernanda Vásquez <82715400+cam-vasquez@users.noreply.github.com>
2022-08-25 23:03:40 -0600

    :zap: improve performance

---
+++ Ver. ONLINE/LogIn2/appReporte.js
@@ -0,0 +1,59 @@
+
+let partner = document.getElementById("partner");
+let persona = document.getElementById("persona");
+let date = document.getElementById("fechaIpt");
+
+$(document).ready(function () {
+    $('select').change(function () {
+        if ($(this).val() == 'action') {
+            //$("#btnEnviar").prop('disabled', true);
+            $("select").prop('disabled', false);
+            $("#year").prop('disabled', false);
+            $("#mes").prop('disabled', false);
+        } else {
+            //$("#btnEnviar").prop('disabled', false);
+            $("select").not(this).prop('disabled', true);
+            $("#year").not(this).prop('disabled', false);
+            $("#mes").not(this).prop('disabled', false);
+        }
+    });
+    checkSend(enviar());
+});
+
+function limpiar(){
+    document.getElementById("partner").disabled = false;
+    document.getElementById("persona").disabled = false;
+}
+
+function checkSend(checker){
+    console.log(checker);
+    if(!checker){
+        $("#form_registro").submit(function(e){
+            e.preventDefault();
+        });
+    }
+    else{
+        $("#form_registro").unbid("submit").submit();
+
+
+    }
+}
+
+function enviar(){
+    if((date.value != "" && partner.value > 0) || (date.value != "" && persona.selectedIndex > 0)){
+        return true;
+    }
+    else
+        return false;
+}
+
+document.getElementById("btnEnviar").onclick = function () {
+    location.href = "index.php";
+    // la url debe ser cambiada
+};
+
+partner.addEventListener("change", () => checkSend(enviar()));
+persona.addEventListener("change", () => checkSend(enviar()));
+date.addEventListener("change", () => checkSend(enviar()));
+
+
